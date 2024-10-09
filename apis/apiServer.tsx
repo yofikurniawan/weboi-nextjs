e352c15505f3d7e390eec2b6ad2d40cb30544b74
Yofi Kurniawan <k.yofi.ky@gmail.com>
2024-10-09 14:12:49 +0700

    Revert "push"
    This reverts commit 24cbdbd61191487087385b0aa8451c286af66ef7.

--- apis/apiServer.tsx
+++ apis/apiServer.tsx
@@ -1,8 +1,8 @@
 export function BaseUrl() {
   // const uri = "https://backend-weboi.superdev.id/api";
-  const uri = "http://127.0.0.1:8000/api";
+  // const uri = "http://127.0.0.1:8000/api";
 
-  // const uri = "https://dev-backend.oganilirkab.go.id/api";
+  const uri = "https://dev-backend.oganilirkab.go.id/api";
 
   https: return uri;
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,7 +42,6 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
       <Script src="/js/bootstrap.bundle.min.js" strategy="lazyOnload" />
       <Script src="/js/wow.min.js" strategy="lazyOnload" />
       <Script src="/js/main.js" strategy="lazyOnload" />
-      
       {getLayout(<Component {...pageProps} />)}
     </>
   );
